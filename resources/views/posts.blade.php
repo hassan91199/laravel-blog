2f4cdf667196adfb1a78d978aba3005cec95ede2
Internee <Internee@LRO-SH-19.vu.edu.pk>
2023-02-14 16:55:20 +0500

    11 completed

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi aut explicabo perspiciatis atque magni a
+            delectus neque aperiam rerum voluptatum distinctio ipsa iusto dicta enim, dolores inventore tenetur quis
+            mollitia.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi aut explicabo perspiciatis atque magni a
+            delectus neque aperiam rerum voluptatum distinctio ipsa iusto dicta enim, dolores inventore tenetur quis
+            mollitia.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi aut explicabo perspiciatis atque magni a
+            delectus neque aperiam rerum voluptatum distinctio ipsa iusto dicta enim, dolores inventore tenetur quis
+            mollitia.</p>
+    </article>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+    // find a post by its slug and pass it to a view called "post"
+
+    $post = Post::find($slug);
+
+    return view('post', [
+        'post' => $post
+    ]);
+
+    // $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    // $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+
+    // if (!file_exists($path)) {
+    //     return redirect('/');
+    // }
+
+    // return view('post', [
+    //     'post' => $post
+    // ]);
+})->where('post', '[A-z_\-]+');
