aa3ac246b1688381665ad7ef5a039aa26a8ba654
hassan91199 <hassanabbas4745@gmail.com>
2023-02-19 13:09:54 +0500

    18 completed

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html>
-<title>My Blog</title>
-<link rel="stylesheet" href="app.css">
+@extends('components.layout')
 
-<body>
+@section('content')
     @foreach ($posts as $post)
-    <article class="{{ $loop->even ? 'foobar' : '' }}">
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
     @endforeach
-</body>
+@endsection
+
