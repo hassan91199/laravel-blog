062edc82860b6be53c91ed0ca75cf007b0894584
hassan91199 <hassanabbas4745@gmail.com>
2023-03-15 11:52:18 +0500

    31 completed

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,16 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
-//    protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = [];
+    protected $with = ['category', 'author'];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
